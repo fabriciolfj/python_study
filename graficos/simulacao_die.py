18956dcf94e1f0e92c0e2b209fa119150a3fc2e0
fabricio jacob <fabrluc@outlook.com>
2023-09-19 22:54:31 -0300

    add

--- graficos/simulacao_die.py
+++ graficos/simulacao_die.py
@@ -4,8 +4,13 @@ die = Die()
 
 random = []
 
-for value in range(100):
+for value in range(1000):
     random.append(die.roll())
 
+frequence = {}
+posicao = range(1, die.num_sides + 1)
+for value in posicao:
+    count = random.count(value)
+    frequence[value] = count
 
-print(random)
+print(frequence)
